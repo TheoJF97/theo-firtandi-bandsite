41199d2ce3a94e0171e6feb91fb528940da99a04
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-05-05 16:35:02 -0400

    Complete JavaScript for adding comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -19,45 +19,140 @@ let commentArray = [
   },
 ];
 
-//Create a function to addComment
-//addEventListener for button COMMENT
+//Grab form element
+const formEl = document.querySelector(".comment-form");
 
-function addComment() {}
+//Listen for submit event via button and store values in object
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = event.target.name.value;
+  const date = new Date().toLocaleDateString();
+  const userComment = event.target.userComment.value;
 
-//Create a function to renderComment
-function renderComments() {
-  const commentSection = document.querySelector(".comment");
+  let newComment = {
+    name: name,
+    date: date,
+    text: userComment,
+  };
 
-  const dividerLine = document.createElement("div");
-  dividerLine.classList.add("divider-line");
-  commentSection.appendChild(dividerLine);
+  addComment(newComment.name, newComment.date, newComment.text);
+});
 
+const largeBox = document.querySelector(".largebox");
+
+//create a function to add the user inputted comment
+function addComment(name, date, text) {
   const commentBox = document.createElement("div");
   commentBox.classList.add("comment-box");
-  commentSection.appendChild(commentBox);
+  largeBox.prepend(commentBox);
 
-  const commentPfp = document.createElement("div");
-  commentPfp.classList.add("comment-pfp");
-  commentBox.appendChild(commentPfp);
+  const commentAvatar = document.createElement("div");
+  commentAvatar.classList.add("comment-avatar");
+  commentBox.prepend(commentAvatar);
 
   const commentContent = document.createElement("div");
   commentContent.classList.add("comment-content");
-  commentBox.appendChild(commentContent);
+  commentBox.prepend(commentContent);
 
   const commentContentHeader = document.createElement("div");
-  commentContentHeader.classList.add("comment-content-header");
-  commentContent.appendChild(commentContentHeader);
+  commentContentHeader.classList.add("comment-header");
+  commentContent.prepend(commentContentHeader);
 
   const commentName = document.createElement("p");
   commentName.classList.add("comment-name");
-  commentContentHeader.appendChild(commentName);
+  commentName.innerText = name;
+  commentContentHeader.prepend(commentName);
 
   const commentDate = document.createElement("span");
   commentDate.classList.add("comment-date");
-  commentContentHeader.appendChild(commentDate);
+  commentDate.innerText = date;
+  commentContentHeader.prepend(commentDate);
 
   const commentMsg = document.createElement("p");
   commentMsg.classList.add("comment-msg");
-  commentContent.appendChild(commentMsg);
+  commentMsg.innerText = text;
+  commentContent.prepend(commentMsg);
+
+  const dividerLine = document.createElement("div");
+  dividerLine.classList.add("divider-line");
+  largeBox.prepend(dividerLine);
 }
 
+//load default comment - function
+function loadDefaultComments(comments) {
+  for (comment of comments) {
+    const dividerLine = document.createElement("div");
+    dividerLine.classList.add("divider-line");
+    largeBox.appendChild(dividerLine);
+
+    const commentBox = document.createElement("div");
+    commentBox.classList.add("comment-box");
+    largeBox.appendChild(commentBox);
+
+    const commentAvatar = document.createElement("div");
+    commentAvatar.classList.add("comment-avatar");
+    commentBox.appendChild(commentAvatar);
+
+    const commentContent = document.createElement("div");
+    commentContent.classList.add("comment-content");
+    commentBox.appendChild(commentContent);
+
+    const commentContentHeader = document.createElement("div");
+    commentContentHeader.classList.add("comment-header");
+    commentContent.appendChild(commentContentHeader);
+
+    const commentName = document.createElement("p");
+    commentName.classList.add("comment-name");
+    commentName.innerText = comment.name;
+    commentContentHeader.appendChild(commentName);
+
+    const commentDate = document.createElement("span");
+    commentDate.classList.add("comment-date");
+    commentDate.innerText = comment.date;
+    commentContentHeader.appendChild(commentDate);
+
+    const commentMsg = document.createElement("p");
+    commentMsg.classList.add("comment-msg");
+    commentMsg.innerText = comment.message;
+    commentContent.appendChild(commentMsg);
+  }
+}
+
+loadDefaultComments(commentArray);
+
+//Create a function to renderComment
+// function renderComments() {
+//   const largeBox = document.querySelector(".comment");
+
+//   const dividerLine = document.createElement("div");
+//   dividerLine.classList.add("divider-line");
+//   largeBox.appendChild(dividerLine);
+
+//   const commentBox = document.createElement("div");
+//   commentBox.classList.add("comment-box");
+//   largeBox.appendChild(commentBox);
+
+//   const commentAvatar = document.createElement("div");
+//   commentAvatar.classList.add("comment-Avatar");
+//   commentBox.appendChild(commentAvatar);
+
+//   const commentContent = document.createElement("div");
+//   commentContent.classList.add("comment-content");
+//   commentBox.appendChild(commentContent);
+
+//   const commentContentHeader = document.createElement("div");
+//   commentContentHeader.classList.add("comment-content-header");
+//   commentContent.appendChild(commentContentHeader);
+
+//   const commentName = document.createElement("p");
+//   commentName.classList.add("comment-name");
+//   commentContentHeader.appendChild(commentName);
+
+//   const commentDate = document.createElement("span");
+//   commentDate.classList.add("comment-date");
+//   commentContentHeader.appendChild(commentDate);
+
+//   const commentMsg = document.createElement("p");
+//   commentMsg.classList.add("comment-msg");
+//   commentContent.appendChild(commentMsg);
+// }
